26514aa50245e4cf8559a1ee90837cc26844a407
Margaret McKenna <margaret.mckenna@nyu.edu>
2012-02-20 19:35:22 -0500

    split out digi requests

--- main.rb
+++ main.rb
@@ -1,102 +1,20 @@
+####
+#
+#    A Simple RemoteControl web app for an XBee network
+#    @author - Margaret McKenna
+#
+####
+
 require 'rubygems'
 require 'sinatra'
 require 'json'
 require 'net/http'
+require './src/digiAPI'
 
 enable :sessions
 
-def digiAuth
-	msg = "Digi does not recognize your credentials"
-	redirect '/logIn?msg='+URI.escape(msg)
-end
-
-
-def digiRequest (_uri,_type,_msg)
-	uri = URI(_uri)
-	req = ''
-	if _type=='post'
-		req = Net::HTTP::Post.new(uri.path)
-		req.content_type = 'text/xml; charset=\"UTF-8\"'
-		req.body(_msg)
-	elsif _type == 'get'
-		req = Net::HTTP::Get.new(uri.request_uri)
-	end
-
-	res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-  		http.request(req)
-	end
-	
-	if res.code!='200'
-		digiAuth()
-	end
-		
-	json = JSON res.body
-	return json
-
-end
-
-def assertXBee(gateway_id,xbee_id)
-	uri = URI("http://developer.idigi.com/ws/sci/.json")
-	msg = '<sci_request version="1.0">
-			  <send_message>
-			    <targets>
-			      <device id="'+gateway_id+'"/>
-			    </targets>
-			    <rci_request version="1.1">
-			      <do_command target="xig">
-			        <at hw_address="'+xbee_id+'" command="D0" value="1" />      
-			        <at hw_address="'+xbee_id+'" command="D1" value="4" />
-			        <at hw_address="'+xbee_id+'" command="D2" value="4" />      
-			        <at hw_address="'+xbee_id+'" command="D3" value="3" />
-			        <at hw_address="'+xbee_id+'" command="D4" value="3" />
-			        <at hw_address="'+xbee_id+'" command="IR" value="30000" />
-			        <at hw_address="'+xbee_id+'" command="IC" value="0x000C" />
-			        <at hw_address="'+xbee_id+'" command="WR" apply="True" />
-			      </do_command>
-			    </rci_request>
-			  </send_message>
-			</sci_request>'
-	json = JSON digiRequest(uri,'post',msg)
-	puts json
-	return json
-
-end
-
-def getXBees (gateway_id)
-	puts gateway_id
-	msg = "<?xml version='1.0' encoding='UTF-8'?><sci_request version='1.0'>
-		  <send_message>
-		    <targets>
-		      <device id='"+gateway_id+"'/>
-		    </targets>
-		    <rci_request version='1.1'>
-		    <do_command target='zigbee'><discover/></do_command>
-		    </rci_request>
-		  </send_message>
-		</sci_request>"
-	uri = URI("http://developer.idigi.com/ws/sci/.json")		
-	
-	json = JSON digiRequest(uri,'post',msg)
-	puts json
-	return json
-end
-
-def getGateways
-	uri = URI('http://developer.idigi.com/ws/DeviceCore/.json')
-	json = JSON digiRequest(uri,'get')
-		
-	gateway_ids = Array.new
-	json['items'].each do |i| 
-		gateway_ids.push(i['devConnectwareId'])
-		getXBees(i['devConnectwareId'])
-	end
-	puts gateway_ids.length
-	return gateway_ids
-end
-
-
-
 configure do
+	## Handle authencation
 	def auth (type)
   		condition do
     		redirect "/logIn" unless send("is_#{type}?")
@@ -106,7 +24,6 @@ end
 
 before do
 	def is_user?
-		puts "before"
       	session[:user_name] != nil
     end    
 end
@@ -124,8 +41,8 @@ end
 
 get '/', :auth => :user do
 	@server = 'http://'+request.env['HTTP_HOST']
-   	#@gateway_ids = getGateways()
-   	@gateway_ids = Array.new
+   	@gateway_ids = getGateways()
+   	#@gateway_ids = Array.new
     #puts "gateways"
     haml :index,  :locals => { :title => 'XBee Remote Control',:log_state => "Log Out",:log_state_url => '/logOut' }
 end
---
+++ src/digiAPI.rb
@@ -0,0 +1,140 @@
+require 'rubygems'
+require 'sinatra'
+require 'json'
+require 'net/http'
+
+
+####
+#
+#    function digiAuth
+#    @params - none
+#    handle Digi authentication failure
+#
+####
+
+def digiAuth
+	msg = "Digi does not recognize your credentials"
+	redirect '/logIn?msg='+URI.escape(msg)
+end
+
+####
+#
+#    function digiRequest
+#    @params - _uri: digi uri
+#			 - _type: post or get
+#            - _msg: empty for get; msg body for post
+#    @return - Digi request response
+#
+####
+
+def digiRequest (_uri,_type,_msg)
+	uri = URI(_uri)
+	req = ''
+	if _type=='post'
+		req = Net::HTTP::Post.new(uri.path)
+		req.content_type = 'text/xml; charset=\"UTF-8\"'
+		req.content_length = _msg.length
+	elsif _type == 'get'
+		req = Net::HTTP::Get.new(uri.request_uri)
+	end
+	
+	req.basic_auth session[:user_name], session[:password]
+	
+	res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+  		http.request(req,_msg)
+	end
+	
+	if res.code!='200'
+		puts res.code, res.msg
+		digiAuth()
+	end
+	return res.body
+
+end
+
+####
+#
+#    function assertXBee
+#    @params - gateway_id, xbee_id
+#    check that all XBee I/Os are configured
+#	 @return - boolean OK
+#
+####
+
+def assertXBee(gateway_id,xbee_id)
+	uri = URI("http://developer.idigi.com/ws/sci/.json")
+	msg = '<sci_request version="1.0">
+			  <send_message>
+			    <targets>
+			      <device id="'+gateway_id+'"/>
+			    </targets>
+			    <rci_request version="1.1">
+			      <do_command target="xig">
+			        <at hw_address="'+xbee_id+'" command="D0" value="1" />      
+			        <at hw_address="'+xbee_id+'" command="D1" value="4" />
+			        <at hw_address="'+xbee_id+'" command="D2" value="4" />      
+			        <at hw_address="'+xbee_id+'" command="D3" value="3" />
+			        <at hw_address="'+xbee_id+'" command="D4" value="3" />
+			        <at hw_address="'+xbee_id+'" command="IR" value="30000" />
+			        <at hw_address="'+xbee_id+'" command="IC" value="0x000C" />
+			        <at hw_address="'+xbee_id+'" command="WR" apply="True" />
+			      </do_command>
+			    </rci_request>
+			  </send_message>
+			</sci_request>'
+	xml = digiRequest(uri,'post',msg)
+	puts xml
+	return xml
+
+end
+
+####
+#
+#    function getXBees
+#    @params - gateway_id
+#    get list of XBees for a gateway
+#	 @return - array of XBee addresses
+#
+####
+
+
+def getXBees (gateway_id)
+	puts gateway_id
+	uri = URI("http://developer.idigi.com/ws/sci/.json")
+	msg = "<sci_request version='1.0'>
+		  <send_message>
+		    <targets>
+		      <device id='"+gateway_id+"'/>
+		    </targets>
+		    <rci_request version='1.1'>
+		    <do_command target='zigbee'><discover/></do_command>
+		    </rci_request>
+		  </send_message>
+		</sci_request>"
+			
+	xml = digiRequest(uri,'post',msg)
+	puts xml
+	return xml
+end
+
+####
+#
+#    function getGateways
+#    @params - none
+#    get list of gateways and associated XBees
+#	 @return - Hash of gateway->XBee addresses
+#
+####
+
+def getGateways
+	uri = URI('http://developer.idigi.com/ws/DeviceCore/.json')
+	json = JSON digiRequest(uri,'get','')
+		
+	gateway_ids = Array.new
+	json['items'].each do |i| 
+		gateway_ids.push(i['devConnectwareId'])
+		getXBees(i['devConnectwareId'])
+	end
+	puts gateway_ids.length
+	return gateway_ids
+end
