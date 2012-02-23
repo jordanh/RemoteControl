2246b5b6e8428fdf69d2048c68f7f8d04a768ff1
Margaret McKenna <margaret.mckenna@nyu.edu>
2012-02-23 07:58:43 -0500

    add authentication back in

--- main.rb
+++ main.rb
@@ -37,8 +37,7 @@ post '/logIn' do
 	redirect '/'
 end
 
-#get '/', :auth => :user do
-get '/' do
+get '/', :auth => :user do
 	@server = 'http://'+request.env['HTTP_HOST']
    	@gateway_ids = getGateways()
     haml :index,  :locals => { :title => 'XBee Remote Control',:log_state => "Log Out",:log_state_url => '/logOut' }
